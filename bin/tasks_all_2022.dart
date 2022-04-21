f09be96e94ad74dde4d9ebb63226f2000f6606ce
Mahsudbek1994 <mikki.12022021@gmail.com>
2022-04-21 12:29:56 +0500

    first commit

---
+++ bin/tasks_all_2022.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+// void main() {
+//
+//   print("Enter your name:");
+//   String? name = stdin.readLineSync();
+//   print("Ente your age:");
+//   int age = int.parse(stdin.readLineSync()!);
+//   int years = 100 - age;
+//   print("$name you have $years years to be 100 years");
+// }
+//
+// void main(){
+//   print("Enter your number:\t");
+//   int number = int.parse(stdin.readLineSync()!);
+//   if(number%2 == 0){
+//     print("your number is odd");
+//   }
+//   else{
+//     print("your number is even");
+//   }
+// }
+// void main(){
+//   List<int>a = [1,1,2,3,5,8,13,21,34,55,89];
+  // for (var i in a){
+  // if( i < 5){
+  //   print(i);
+  // }
+  // }
+  // print([for(var i in a)if (i>5)i]);
+
+// }
+
+// void main(){
+//
+//   print('Enter your number');
+//   int number = int.parse(stdin.readLineSync()!);
+//   for ( var i = 1; i<=number; i++) {
+//     if (number % i == 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// void main(){
+//
+//   List <int> a=[1,2,1,3,5,8,13,21,23,34,55,89];
+//   List <int> b=[1,2,1,3,5,8,13,21,23,34,55,89];
+  // Set<int>c ={};
+  // for(var i in a){
+  //   for( var j in b){
+  //     if (i == j){
+  //       c.add(i);
+  //     }
+  //   }
+  // }
+  // print(c.toList());
+  //ONE line using in intersections
+  // print(Set.from(a).intersection(Set.from(b)).toList());
+// }
+//
+// void main(){
+//   print('Please give a word');
+//   String? input = stdin.readLineSync();
+//   String? revInput = input!.split("").reversed.join('');
+//   input == revInput
+//   ? print('The word is palindrome')
+//       : print("The word is not a palindrome");
+// }
+// void main (){
+//   List<int> a = [1,4,9,16,25,36,49,64,81,100];
+//   int i = 0;
+  // List<int> j = [];
+  // for(var e in a){
+  //   if(++i%2 == 0){
+  //     j.add(e);
+  //   }
+  // }
+  // print(j);
+  // print([for(var e in a)if(++i % 2 == 0)e]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
